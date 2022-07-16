81b4444fd6d97ada2b05e21fb783cabd23a593e8
emmanuelorobinson <manuelejeay@gmail.com>
2022-07-16 23:31:05 +0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Emmanuel Robinson on 16/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
